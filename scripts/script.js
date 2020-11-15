e6e190fe216453a3f7f6321acd6f94d226dc7943
ChaplinGleb <ChaplinGleb@users.noreply.github.com>
2020-11-15 23:08:30 +0300

    create second screen + add js + add attemps

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,72 @@
+/* 
+СЛОЖНОСТЬ            ПОПЫТОК
+Легко: 1-100         10
+Средне: 1-1 000      15
+Сложно: 1-10 000     20
+Эксперт: 1-100 000   25
+*/
+
+const btnHits = document.getElementById('getHits')
+let numOfHits
+
+
+
 $(function(){
    let j = 0
    $('#checkbox1').click(function(){
       if (j == 0){
-         $('.container').toggleClass('container-full')
+         $('.setting').toggleClass('setting-full')
          setTimeout(function(){
             $('#hits-block').toggleClass('hits-block-full')
          }, 300)
+         $('#getHits').toggleClass('play__btn-hit-show')
+         $('#blockHits').toggleClass('play__hits-block_show')
+         
          j = 1
       }else{
             $('#hits-block').toggleClass('hits-block-full')
             setTimeout(function(){
-               $('.container').toggleClass('container-full')
+               $('.setting').toggleClass('setting-full')
             }, 300)
+            $('#getHits').toggleClass('play__btn-hit-show')
+            $('#blockHits').toggleClass('play__hits-block_show')
             j = 0
       }
    })
+   $('#start').click(function(){
+      $('#setting').fadeOut()
+      setTimeout(function(){
+         $('#play').fadeIn()
+      }, 500)
+   })
 })
-
-
 
 let all = document.querySelectorAll('.difficulty-block');
 for (let a = 0; a < all.length; a++){ 
-    let radios = all[a].querySelectorAll('.difficulty-block__input');
-    let i = 1;
-    all[a].style.setProperty('--options',radios.length);
-    radios.forEach((input)=>{
-        input.setAttribute('data-pos',i);
-        input.addEventListener('click',(e)=>{
-            all[a].style.setProperty('--options-active',e.target.getAttribute('data-pos'));
-        });
-        i++;
-    });
-}; 
+   let radios = all[a].querySelectorAll('.difficulty-block__input');
+   let i = 1;
+   all[a].style.setProperty('--options',radios.length);
+   radios.forEach((input)=>{
+      input.setAttribute('data-pos',i);
+      input.addEventListener('click',(e)=>{
+         all[a].style.setProperty('--options-active',e.target.getAttribute('data-pos'));
+         numOfHits = e.target.getAttribute('data-pos')
+         btnHits.innerHTML = `Подсказка (${numOfHits} шт.)`
+      });
+      i++;
+   });
+};
 
 
+
+function getHits(){
+   if(btnHits.innerHTML == "Подсказка (1 шт.)"){
+      btnHits.setAttribute('disabled', 'true')
+      btnHits.innerHTML = 'Подсказок нет'
+   }else{
+      btnHits.innerHTML = `Подсказка (${--numOfHits} шт.)`
+   }
+}
 
 
 
